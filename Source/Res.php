c39e7d30d399ac4af4b6e39a5a54d16a196a9823
Mega.D <phamanhduc30012005@gmail.com>
2024-12-10 23:44:28 +0700

    Management

---
+++ Source/Res.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document Management</title>
+    <link rel="stylesheet" href="Res.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <div class="login-div">
+        <form class="login-form" action="">
+            <h1>ĐĂNG NHẬP</h1>    
+            <div class="form-group">
+                <div class="input-container">
+                    <input type="text" id="username" name="username" placeholder="Nhập tên đăng nhập">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="input-container">
+                    <i class="icon fa fa-lock"></i>
+                    <input type="password" id="password" name="password" placeholder="Nhập mật khẩu">
+                </div>
+            </div>
+            <button type="submit"><h3>ĐĂNG NHẬP</h3></button> 
+        </form>
+    </div>
+</body>
+</html>
