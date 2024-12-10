eb1f866eaecabfea8b1c4f6c23fcb7d8efd2d627
Mega.D <phamanhduc30012005@gmail.com>
2024-12-11 01:46:39 +0700

    Management

---
+++ Homepage.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document Management</title>
+    <link rel="stylesheet" href="Homepage.css">
+</head>
+<body>
+    <div class="body">
+        <div class="left_nav">
+            <img class="logo_only" src="Source/img/UTT_Logo_Only.png" width="100px">
+            
+            <ul>
+                <li><a href="">Quản lý văn bản đi</a></li>
+                <li>Quản lý văn bản đến</li>
+                <li>3</li>
+                <li>4</li>
+            </ul>
+        </div>
+        
+        <div class="content">
+    </div>
+</body>
+</html>
--- Login.php
+++ Login.php
@@ -4,22 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document Management</title>
-    <link rel="stylesheet" href="Res.css">
+    <link rel="stylesheet" href="Login.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
     <div class="login-div">
-        <form class="login-form" action="">
+        <form class="login-form" action="homepage.php" method="post">
+            <img class="logo_text" src="Source/img/UTT_Logo.png" alt="" width="150px">
             <h1>ĐĂNG NHẬP</h1>    
             <div class="form-group">
                 <div class="input-container">
-                    <input type="text" id="username" name="username" placeholder="Nhập tên đăng nhập">
+                    <input class="form_control" type="text" id="username" name="username" placeholder="Nhập tên đăng nhập">
                 </div>
             </div>
             <div class="form-group">
                 <div class="input-container">
-                    <i class="icon fa fa-lock"></i>
-                    <input type="password" id="password" name="password" placeholder="Nhập mật khẩu">
+                    <input class="form_control type="password" id="password" name="password" placeholder="Nhập mật khẩu">
                 </div>
             </div>
             <button type="submit"><h3>ĐĂNG NHẬP</h3></button> 
